ccbd5cc80d73864f716b4114c592e52f1d8896ff
opexcode <xovmck@gmail.com>
2022-11-06 17:46:26 +1100

    Initial Commit

---
+++ MidiKit/MidiKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  MidiKitApp.swift
+//  MidiKit
+//
+//  Created by   imac on 06.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct MidiKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
